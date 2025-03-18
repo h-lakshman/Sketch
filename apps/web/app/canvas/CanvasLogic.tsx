9e21b4ab73a2d47860cce10234c7adc89561209b
h-lakshman <lakshman.vaibhav007@gmail.com>
2025-03-18 16:38:08 +0530

    -added clear canvas for every new shape render -all existing shapes render will be cleared and render again before new shape

--- apps/web/app/canvas/CanvasLogic.tsx
+++ apps/web/app/canvas/CanvasLogic.tsx
@@ -1,5 +1,18 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
+
+enum ShapeType {
+  Rectangle = "rectangle",
+}
+
+interface Rectangle {
+  type: ShapeType.Rectangle;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+type Shapes = Rectangle;
 
 const drawRect = (
   ctx: CanvasRenderingContext2D,
@@ -8,10 +21,22 @@ const drawRect = (
   width: number,
   height: number
 ) => {
-  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
   ctx.strokeRect(x, y, width, height);
   ctx.strokeStyle = "white";
   ctx.lineWidth = 2;
+};
+
+const clearCanvas = (ctx: CanvasRenderingContext2D) => {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+};
+
+const renderShapes = (ctx: CanvasRenderingContext2D, shapes: Shapes[]) => {
+  clearCanvas(ctx);
+  shapes.forEach((shape) => {
+    if (shape.type === ShapeType.Rectangle) {
+      drawRect(ctx, shape.x, shape.y, shape.width, shape.height);
+    }
+  });
 };
 
 const CanvasPage = () => {
@@ -19,11 +44,13 @@ const CanvasPage = () => {
   const isDrawingRef = useRef(false);
   const startXRef = useRef(0);
   const startYRef = useRef(0);
-
+  const existingShapes = useRef<Shapes[]>([]);
   const resizeCanvas = () => {
     const canvas = canvasRef.current;
-    if (!canvas) return;
-
+    if (!canvas) {
+      console.log("canvas not found");
+      return;
+    }
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
   };
@@ -31,11 +58,17 @@ const CanvasPage = () => {
   useEffect(() => {
     const canvas = canvasRef.current;
 
-    if (!canvas) return;
+    if (!canvas) {
+      console.log("canvas not found");
+      return;
+    }
     resizeCanvas();
 
     const ctx = canvas.getContext("2d");
-    if (!ctx) return;
+    if (!ctx) {
+      console.log("canvas context not found");
+      return;
+    }
 
     const handleMouseDown = (e: MouseEvent) => {
       isDrawingRef.current = true;
@@ -47,18 +80,27 @@ const CanvasPage = () => {
       if (isDrawingRef.current) {
         const width = e.clientX - startXRef.current;
         const height = e.clientY - startYRef.current;
+        renderShapes(ctx, existingShapes.current);
         drawRect(ctx, startXRef.current, startYRef.current, width, height);
       }
     };
 
     const handleMouseUp = (e: MouseEvent) => {
       isDrawingRef.current = false;
+      const width = e.clientX - startXRef.current;
+      const height = e.clientY - startYRef.current;
+      existingShapes.current.push({
+        type: ShapeType.Rectangle,
+        x: startXRef.current,
+        y: startYRef.current,
+        width,
+        height,
+      });
     };
-
     canvas.addEventListener("mousedown", handleMouseDown);
     canvas.addEventListener("mousemove", handleMouseMove);
     canvas.addEventListener("mouseup", handleMouseUp);
-    window.addEventListener("resize", resizeCanvas); 
+    window.addEventListener("resize", resizeCanvas);
     return () => {
       canvas.removeEventListener("mousedown", handleMouseDown);
       canvas.removeEventListener("mousemove", handleMouseMove);
