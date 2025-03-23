d5320ffb4f7f2fa8c99712432b01c5828d9cae26
h-lakshman <lakshman.vaibhav007@gmail.com>
2025-03-23 20:46:38 +0530

    added colors to each shape

---
+++ packages/db/prisma/migrations/20250323150317_added/migration.sql
@@ -0,0 +1,20 @@
+-- AlterTable
+ALTER TABLE "Diamond" ADD COLUMN     "color" TEXT NOT NULL DEFAULT '#ffffff';
+
+-- AlterTable
+ALTER TABLE "Ellipse" ADD COLUMN     "color" TEXT NOT NULL DEFAULT '#ffffff';
+
+-- AlterTable
+ALTER TABLE "Line" ADD COLUMN     "color" TEXT NOT NULL DEFAULT '#ffffff';
+
+-- AlterTable
+ALTER TABLE "LineWithArrow" ADD COLUMN     "color" TEXT NOT NULL DEFAULT '#ffffff';
+
+-- AlterTable
+ALTER TABLE "Pen" ADD COLUMN     "color" TEXT NOT NULL DEFAULT '#ffffff';
+
+-- AlterTable
+ALTER TABLE "Rectangle" ADD COLUMN     "color" TEXT NOT NULL DEFAULT '#ffffff';
+
+-- AlterTable
+ALTER TABLE "Text" ADD COLUMN     "color" TEXT NOT NULL DEFAULT '#ffffff';
